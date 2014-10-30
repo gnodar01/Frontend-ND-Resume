32c5a92499d01cce87cb6312fdaaee97638cf804
gnodar01 <gnodar01@gmail.com>
2014-10-30 01:00:30 -0400

    added name, email variables; added aesome thought vairable, replaced awesome with fun, and appended it to #main

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,7 @@
-$("#main").append("Nodari Gogoberidze")
+var myName = "Nodari Gogoberidze";
+var myEmail = "gnodar01@gmail.com";
+
+var awesomeThought = "I am " + myName + " and I am AWESOME!";
+var funThoughts = awesomeThought.replace("AWESOME","FUN");
+
+$("#main").append(funThoughts);
