141873f8b4ed9ef97be31449d201e84cf4fd2d4d
gnodar01 <gnodar01@gmail.com>
2014-11-04 23:36:10 -0500

    Change a lot. I need to keep better track of changes fron now on.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,16 +17,17 @@ var work = {
 		{
 			"title": "Market Research Analyst",
 			"employer": "University of Central Florida: Office of Research and Commercialization",
-			"yearsWorked": 1,
+			"yearsWorked": "1 year",
 			"workLocation": "Orlando, Florida",
-			"description": "none"
+			"jobDescription": "Market research for strategic decision making"
 		},
 		{
 			"title": "Market Research Analyst",
 			"employer": "Invitation Homes - a subsidiary of The Blackstone Group",
-			"yearsWorked": 1,
+			"yearsWorked": "1 year",
 			"workLocation": "Orlando, Florida",
-			"description": "none"
+			"jobDescription": "Determine residential real estate market values, rent values, rehab estimates"
+			+ " and overall optimal purchase price valuation"
 		}
 	]
 }
@@ -87,7 +88,52 @@ var education = {
 	]
 }
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
+function displayWork() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].yearsWorked);
+		var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].workLocation);
+		var formattedJobDescription = HTMLworkDescription.replace("%data%", work.jobs[job].jobDescription);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+		$(".work-entry:last").append(formattedEmployerTitle);
+		$(".work-entry:last").append(formattedWorkDates);
+		$(".work-entry:last").append(formattedWorkLocation);
+		$(".work-entry:last").append(formattedJobDescription);
+	}
+
+	$("#skillsChart").append(HTMLskillsStart);
+	for (skill in bio.skills) {
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+		$(".flex-box").append(formattedSkill);
+	}
+}
+displayWork();
+
+$(document).click(function(loc) { 
+	var x = loc.pageX;
+	var y = loc.pageY;
+
+	logClicks(x,y);
+});
+
+
+$("#main").append(internationalizeButton);
+
+function inName(firstLast) {
+	var splitName = firstLast.split(" "),
+	 changeFirstName = splitName[0].slice(0,1).toUpperCase() + splitName[0].slice(1).toLowerCase(),
+	 changeLastName = splitName[1].toUpperCase(),
+	 combineFirstLast = changeFirstName + " " + changeLastName;
+	return combineFirstLast;
+}
+
+console.log(inName("nodari gogoberidze"));
+
+/*var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedContact = HTMLcontactGeneric.replace("%data", bio.contacts);
 var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
@@ -106,4 +152,4 @@ $("#letsConnect").append(formattedMobile);
 $("#letsConnect").append(formattedGithub);
 $("#letsConnect").append(formattedCity);
 $("#header").append(formattedBioPic);
-$("#header").append(formattedWelcomeMessage);
+$("#header").append(formattedWelcomeMessage);*/
